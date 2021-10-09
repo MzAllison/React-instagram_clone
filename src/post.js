a381d1144f29a188014880d9f5a6843bfcfd4b83
MzAllison <isituaho@gmail.com>
2021-10-09 07:00:20 +0100

    Header

---
+++ src/post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function post() {
+    return (
+        <div>
+            <h3>Username</h3>
+            {/* header -> avatar + usernam */}
+            {/* image */}
+            {/* username + caption */}
+        </div>
+    )
+}
+
+export default post
